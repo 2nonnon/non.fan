0a19175c78e3a801d36c36dac0b0a1729d805141
2nonnon <lanpangzi0205@gmail.com>
2025-03-10 22:21:00 +0800

    some

--- apps/web/components/layout/header.tsx
+++ apps/web/components/layout/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 		<header className="sticky top-0 z-10 backdrop-blur">
 			<div className="flex items-center justify-between gap-4 h-24 px-6 py-3 max-w-screen-xl mx-auto">
 				<Link className="font-bold" href="/">
-					LOGO.svg
+					施工中...
 				</Link>
 
 				<ThemeChanger />
