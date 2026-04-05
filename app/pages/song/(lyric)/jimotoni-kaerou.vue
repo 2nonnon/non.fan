32574f7f397e0f37987dd2920a7b0964de24aaf6
fuyu <contact@non.fan>
2026-04-05 21:20:08 +0800

    歌词补充

---
+++ app/pages/song/(lyric)/jimotoni-kaerou.vue
@@ -0,0 +1,63 @@
+<template>
+  <FLyricBase track-id="jimotoni-kaerou">
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>に&emsp;<ruby>帰<rp>(</rp><rt>かえ</rt><rp>)</rp></ruby>ろう&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>で&emsp;<ruby>逢<rp>(</rp><rt>あ</rt><rp>)</rp></ruby>おう</p>
+    <p>回到故乡吧 在故乡重逢吧</p>
+
+    <p>あなたの&emsp;<ruby>故郷<rp>(</rp><rt>ふるさと</rt><rp>)</rp></ruby>&emsp;<ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby>の&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby></p>
+    <p>你的故乡 我的家乡</p>
+
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>に&emsp;<ruby>帰<rp>(</rp><rt>かえ</rt><rp>)</rp></ruby>ろう</p>
+    <p>故乡 故乡 回到故乡吧</p>
+
+    <p><ruby>好<rp>(</rp><rt>す</rt><rp>)</rp></ruby>きです&emsp;<ruby>先輩<rp>(</rp><rt>せんぱい</rt><rp>)</rp></ruby>&emsp;<ruby>覚<rp>(</rp><rt>おぼ</rt><rp>)</rp></ruby>えてますか?</p>
+    <p>我喜欢你 前辈 你还记得吗？</p>
+
+    <p><ruby>朝礼<rp>(</rp><rt>ちょうれい</rt><rp>)</rp></ruby>で&emsp;<ruby>倒<rp>(</rp><rt>たお</rt><rp>)</rp></ruby>れた&emsp;<ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby></p>
+    <p>在晨会上晕倒的我</p>
+
+    <p><ruby>都会<rp>(</rp><rt>とかい</rt><rp>)</rp></ruby>では&emsp;<ruby>先輩<rp>(</rp><rt>せんぱい</rt><rp>)</rp></ruby>&emsp;<ruby>訛<rp>(</rp><rt>なま</rt><rp>)</rp></ruby>ってますか?</p>
+    <p>在城市里 前辈 说话有口音吗？</p>
+
+    <p>お<ruby>寿司<rp>(</rp><rt>すし</rt><rp>)</rp></ruby>を&emsp;「おすす」と&emsp;<ruby>言<rp>(</rp><rt>い</rt><rp>)</rp></ruby>ってた&emsp;<ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby></p>
+    <p>曾把寿司说成“Osusu”的我</p>
+
+    <p><ruby>第二<rp>(</rp><rt>だいに</rt><rp>)</rp></ruby>ボタンは&emsp;<ruby>捨<rp>(</rp><rt>す</rt><rp>)</rp></ruby>てました&emsp;<ruby>腹<rp>(</rp><rt>はら</rt><rp>)</rp></ruby>いせに</p>
+    <p>第二颗纽扣 被我为了泄愤扔掉了</p>
+
+    <p><ruby>第三京浜<rp>(</rp><rt>だいさんけいひん</rt><rp>)</rp></ruby>&emsp;<ruby>乗<rp>(</rp><rt>の</rt><rp>)</rp></ruby>りました&emsp;<ruby>川崎方面<rp>(</rp><rt>かわさきほうめん</rt><rp>)</rp></ruby>へ</p>
+    <p>开上了第三京滨道路 朝着川崎方向</p>
+
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>に&emsp;<ruby>帰<rp>(</rp><rt>かえ</rt><rp>)</rp></ruby>ろう&emsp;(<ruby>好<rp>(</rp><rt>す</rt><rp>)</rp></ruby>きよ)</p>
+    <p>回到故乡吧（喜欢你哦）</p>
+
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>で&emsp;<ruby>逢<rp>(</rp><rt>あ</rt><rp>)</rp></ruby>おう&emsp;(<ruby>先輩<rp>(</rp><rt>せんぱい</rt><rp>)</rp></ruby>)</p>
+    <p>在故乡重逢吧（前辈）</p>
+
+    <p>あなたの&emsp;<ruby>故郷<rp>(</rp><rt>ふるさと</rt><rp>)</rp></ruby>&emsp;(がばい)</p>
+    <p>你的故乡（非常/佐贺方言）</p>
+
+    <p><ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby>の&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;(でーじ)</p>
+    <p>我的家乡（非常/冲绳方言）</p>
+
+    <p><ruby>駅前<rp>(</rp><rt>えきまえ</rt><rp>)</rp></ruby>&emsp;コンビニ&emsp;<ruby>駐車場<rp>(</rp><rt>ちゅうしゃじょう</rt><rp>)</rp></ruby></p>
+    <p>车站前 便利店 停车场</p>
+
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>に&emsp;<ruby>帰<rp>(</rp><rt>かえ</rt><rp>)</rp></ruby>ろう</p>
+    <p>故乡 故乡 回到故乡吧</p>
+
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>に&emsp;<ruby>帰<rp>(</rp><rt>かえ</rt><rp>)</rp></ruby>ろう&emsp;(<ruby>埼玉<rp>(</rp><rt>さいたま</rt><rp>)</rp></ruby>)</p>
+    <p>回到故乡吧（埼玉）</p>
+
+    <p><ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>で&emsp;<ruby>逢<rp>(</rp><rt>あ</rt><rp>)</rp></ruby>おう&emsp;(<ruby>仙台<rp>(</rp><rt>せんだい</rt><rp>)</rp></ruby>)</p>
+    <p>在故乡重逢吧（仙台）</p>
+
+    <p>あなたの&emsp;<ruby>故郷<rp>(</rp><rt>ふるさと</rt><rp>)</rp></ruby>&emsp;(<ruby>佐賀<rp>(</rp><rt>さが</rt><rp>)</rp></ruby>)</p>
+    <p>你的故乡（佐贺）</p>
+
+    <p><ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby>の&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;(<ruby>沖縄<rp>(</rp><rt>おきなわ</rt><rp>)</rp></ruby>)</p>
+    <p>我的家乡（冲绳）</p>
+
+    <p><ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby>の&emsp;<ruby>地元<rp>(</rp><rt>じもと</rt><rp>)</rp></ruby>&emsp;(ブラジル)&emsp;(<ruby>岩手<rp>(</rp><rt>いわて</rt><rp>)</rp></ruby>)</p>
+    <p>我的家乡（巴西）（岩手）</p>
+  </FLyricBase>
+</template>
---
+++ app/pages/song/(lyric)/koyomi-no-ue-dewa-december.vue
@@ -0,0 +1,69 @@
+<template>
+  <FLyricBase track-id="koyomi-no-ue-dewa-december">
+    <p><ruby>暦<rp>(</rp><rt>こよみ</rt><rp>)</rp></ruby>の&emsp;<ruby>上<rp>(</rp><rt>うえ</rt><rp>)</rp></ruby>ではディセンバー&emsp;でも&emsp;ハートはサバイバー</p>
+    <p>历法上已是十二月 但内心仍是幸存者</p>
+
+    <p><ruby>師走<rp>(</rp><rt>しわす</rt><rp>)</rp></ruby>は&emsp;<ruby>忙<rp>(</rp><rt>いそが</rt><rp>)</rp></ruby>しい&emsp;<ruby>町<rp>(</rp><rt>まち</rt><rp>)</rp></ruby>は&emsp;<ruby>慌<rp>(</rp><rt>あわ</rt><rp>)</rp></ruby>ただしい</p>
+    <p>腊月时节正忙碌 街道气氛也匆匆</p>
+
+    <p>だけど&emsp;<ruby>虚<rp>(</rp><rt>むな</rt><rp>)</rp></ruby>しい&emsp;そこは&emsp;デリカシー</p>
+    <p>然而 却感到空虚 这便是 所谓微妙的感受</p>
+
+    <p><ruby>果<rp>(</rp><rt>は</rt><rp>)</rp></ruby>てしなく&emsp;ラララ&emsp;<ruby>貪欲<rp>(</rp><rt>どんよく</rt><rp>)</rp></ruby>&emsp;<ruby>貪欲<rp>(</rp><rt>どんよく</rt><rp>)</rp></ruby></p>
+    <p>永无止境 啦啦啦 贪婪 贪婪</p>
+
+    <p>クリスマスを&emsp;<ruby>過<rp>(</rp><rt>す</rt><rp>)</rp></ruby>ごす&emsp;<ruby>相手<rp>(</rp><rt>あいて</rt><rp>)</rp></ruby>と</p>
+    <p>和一起度过圣诞节的对象</p>
+
+    <p>あてどなく&emsp;ラララ&emsp;<ruby>暗躍<rp>(</rp><rt>あんやく</rt><rp>)</rp></ruby>&emsp;<ruby>暗躍<rp>(</rp><rt>あんやく</rt><rp>)</rp></ruby></p>
+    <p>漫无目的 啦啦啦 暗中行动 暗中行动</p>
+
+    <p>お<ruby>正月<rp>(</rp><rt>しょうがつ</rt><rp>)</rp></ruby>まで&emsp;<ruby>一緒<rp>(</rp><rt>いっしょ</rt><rp>)</rp></ruby>に&emsp;いたくない</p>
+    <p>并不想一直待到过年</p>
+
+    <p><ruby>暦<rp>(</rp><rt>こよみ</rt><rp>)</rp></ruby>の&emsp;<ruby>上<rp>(</rp><rt>うえ</rt><rp>)</rp></ruby>ではディセンバー&emsp;でも&emsp;ハートはサバイバー</p>
+    <p>历法上已是十二月 但内心仍是幸存者</p>
+
+    <p><ruby>今宵<rp>(</rp><rt>こよい</rt><rp>)</rp></ruby>の&emsp;<ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby>はディセンバー&emsp;さまよう&emsp;<ruby>気分<rp>(</rp><rt>きぶん</rt><rp>)</rp></ruby>は&emsp;ハンター</p>
+    <p>今晚的我已入十二月 徘徊的心情正如猎手</p>
+
+    <p><ruby>来年<rp>(</rp><rt>らいねん</rt><rp>)</rp></ruby>の&emsp;<ruby>恋<rp>(</rp><rt>こい</rt><rp>)</rp></ruby>なんて&emsp;<ruby>鬼<rp>(</rp><rt>おに</rt><rp>)</rp></ruby>が&emsp;<ruby>笑<rp>(</rp><rt>わら</rt><rp>)</rp></ruby>うわ</p>
+    <p>谈论来年的恋爱 连鬼都会发笑哦</p>
+
+    <p>A!M!E!<ruby>横<rp>(</rp><rt>よこ</rt><rp>)</rp></ruby>!アメ&nbsp;YO!&nbsp;YO!&nbsp;YO!&nbsp;<ruby>横<rp>(</rp><rt>よこ</rt><rp>)</rp></ruby>!</p>
+    <p>A!M!E!横!阿美&nbsp;YO!&nbsp;YO!&nbsp;YO!&nbsp;横!</p>
+
+    <p><ruby>上野<rp>(</rp><rt>うえの</rt><rp>)</rp></ruby>から&emsp;<ruby>御徒町<rp>(</rp><rt>おかちまち</rt><rp>)</rp></ruby>に&emsp;<ruby>棲息<rp>(</rp><rt>せいそく</rt><rp>)</rp></ruby>&emsp;<ruby>絶滅危惧種<rp>(</rp><rt>ぜつめつきぐしゅ</rt><rp>)</rp></ruby>&emsp;<ruby>下町<rp>(</rp><rt>したまち</rt><rp>)</rp></ruby>アイドル</p>
+    <p>栖息在上野到御徒町一带 濒危物种 老街偶像</p>
+
+    <p><ruby>会<rp>(</rp><rt>あ</rt><rp>)</rp></ruby>いたい&emsp;でも&emsp;<ruby>会<rp>(</rp><rt>あ</rt><rp>)</rp></ruby>えない&emsp;<ruby>脳内自由恋愛集団<rp>(</rp><rt>のうないじゆうれんあいしゅうだん</rt><rp>)</rp></ruby></p>
+    <p>好想见你 但不能相见 脑内自由恋爱集团</p>
+
+    <p><ruby>男子禁制<rp>(</rp><rt>だんしきんせい</rt><rp>)</rp></ruby>&emsp;<ruby>選手宣誓<rp>(</rp><rt>せんしゅせんせい</rt><rp>)</rp></ruby>&emsp;<ruby>毎日会<rp>(</rp><rt>まいにちあ</rt><rp>)</rp></ruby>いたい&emsp;<ruby>下町<rp>(</rp><rt>したまち</rt><rp>)</rp></ruby>アイドル</p>
+    <p>男生禁止 选手宣誓 每天都想见的 老街偶像</p>
+
+    <p>We&nbsp;are&nbsp;アメ<ruby>横女学園<rp>(</rp><rt>よこじょがくえん</rt><rp>)</rp></ruby>!!&emsp;You&nbsp;are&nbsp;アメ<ruby>横女学園<rp>(</rp><rt>よこじょがくえん</rt><rp>)</rp></ruby>!!</p>
+    <p>We are 阿美横女学园!! You are 阿美横女学园!!</p>
+
+    <p><ruby>果<rp>(</rp><rt>は</rt><rp>)</rp></ruby>てしなく&emsp;ラララ&emsp;<ruby>貪欲<rp>(</rp><rt>どんよく</rt><rp>)</rp></ruby>&emsp;<ruby>貪欲<rp>(</rp><rt>どんよく</rt><rp>)</rp></ruby></p>
+    <p>永无止境 啦啦啦 贪婪 贪婪</p>
+
+    <p><ruby>終<rp>(</rp><rt>お</rt><rp>)</rp></ruby>わらない&emsp;<ruby>恋<rp>(</rp><rt>こい</rt><rp>)</rp></ruby>の&emsp;<ruby>大掃除<rp>(</rp><rt>おおそうじ</rt><rp>)</rp></ruby></p>
+    <p>永不结束的 恋爱大扫除</p>
+
+    <p><ruby>去<rp>(</rp><rt>さ</rt><rp>)</rp></ruby>り&emsp;<ruby>際<rp>(</rp><rt>ぎわ</rt><rp>)</rp></ruby>は&emsp;ラララ&emsp;<ruby>淡泊<rp>(</rp><rt>たんぱく</rt><rp>)</rp></ruby>&emsp;<ruby>淡泊<rp>(</rp><rt>たんぱく</rt><rp>)</rp></ruby></p>
+    <p>离去之时 啦啦啦 恬淡 恬淡</p>
+
+    <p><ruby>連絡<rp>(</rp><rt>れんらく</rt><rp>)</rp></ruby>しないで&emsp;<ruby>喪中<rp>(</rp><rt>もちゅう</rt><rp>)</rp></ruby>なんだから</p>
+    <p>请别联系我 毕竟正在服丧期间（指告别旧恋情）</p>
+
+    <p><ruby>暦<rp>(</rp><rt>こよみ</rt><rp>)</rp></ruby>の&emsp;<ruby>上<rp>(</rp><rt>うえ</rt><rp>)</rp></ruby>ではディセンバー&emsp;でも&emsp;ハートはサバイバー</p>
+    <p>历法上已是十二月 但内心仍是幸存者</p>
+
+    <p><ruby>去年<rp>(</rp><rt>きょねん</rt><rp>)</rp></ruby>の&emsp;<ruby>私<rp>(</rp><rt>わたし</rt><rp>)</rp></ruby>はリメンバー&emsp;ただよう&emsp;<ruby>気分<rp>(</rp><rt>きぶん</rt><rp>)</rp></ruby>はロンリー</p>
+    <p>去年的我 记忆犹新 漂浮的心情 如此孤独</p>
+
+    <p><ruby>歩<rp>(</rp><rt>ある</rt><rp>)</rp></ruby>き&emsp;<ruby>疲<rp>(</rp><rt>つか</rt><rp>)</rp></ruby>れて&emsp;<ruby>膝<rp>(</rp><rt>ひざ</rt><rp>)</rp></ruby>が&emsp;<ruby>笑<rp>(</rp><rt>わら</rt><rp>)</rp></ruby>うわ</p>
+    <p>走到精疲力竭 膝盖都在打颤了</p>
+  </FLyricBase>
+</template>
